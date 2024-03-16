a92f0f2727f68f0fcfcd28fad46f8113c0b8213b
yChenPerimeter <ychen@perimetermed.com>
2024-03-16 16:30:57 -0400

    Create requirements.txt for altas

--- options/base_options.py
+++ options/base_options.py
@@ -41,7 +41,7 @@ class BaseOptions():
         parser.add_argument('--dataset_mode', type=str, default='aligned', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
         parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
         parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
-        parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
+        parser.add_argument('--num_threads', default=1, type=int, help='# threads for loading data')
         parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
         parser.add_argument('--data_bit', type = int, default = 8, help="# of bits per pixel")
         
