0ad668cd487fadad2c3cc4a7136e6146329544a2
yChenPerimeter <ychen@perimetermed.com>
2024-07-17 17:35:20 -0400

    buggy in this branch would mamage later

--- options/base_options.py
+++ options/base_options.py
@@ -74,7 +74,7 @@ class BaseOptions():
         parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
         parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}')
         # wandb parameters
-        #True
+        #True  
         parser.add_argument('--use_wandb',  default=True ,action='store_true', help='if specified, then init wandb logging')
         parser.add_argument('--wandb_project_name', type=str, default='CycleGAN-and-pix2pix', help='specify wandb project name')
         self.initialized = True
