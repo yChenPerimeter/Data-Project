89dd8468b6d882c27a6bc3978ed830aabe155aa0
ychen <ychen@perimetermed.com>
2024-10-16 14:38:27 -0400

    updating

--- test_scripted_model.py
+++ test_scripted_model.py
@@ -1,9 +1,3 @@
-"""
-File Name: test_scriptedModels.py
-Description: 
-    Quick denoise test using scripted CycleGAN model with configurable inference type
-"""
-
 import cv2
 import numpy as np
 import os
@@ -11,6 +5,8 @@ import torch
 import torchvision as tv
 import matplotlib.pyplot as plt
 from PIL import Image
+import shutil
+import zipfile
 
 from torch.profiler import profile, record_function, ProfilerActivity
 import sys
@@ -18,7 +14,8 @@ import sys
 # Path creator function that checks if the path exists, if not, creates one
 def path_creator(path):
     if not os.path.exists(path):
-        os.mkdir(path)
+        os.makedirs(path)
+
 
 def tensor2im(input_image, imtype=np.uint8):
     """Converts a Tensor array into a numpy image array."""
@@ -35,36 +32,42 @@ def tensor2im(input_image, imtype=np.uint8):
         image_numpy = input_image
     return image_numpy.astype(imtype)
 
+
 class Constants:
     """Constants used in the processing of the data."""
     def __init__(self):
-        self.destination_folder = r"/home/david/workingDIR/datasets_pro_test/denoised_1x" # replace with your own destination path
-        self.root_test_folder_A = r"/home/david/workingDIR/datasets_pro_test/testset_A" # replace with your own domain A path
-        self.root_test_folder_B = r"/home/david/workingDIR/datasets_pro_test/testset_B" # replace with your own domain B path
-        self.model_path_G_A = r"/home/david/workingDIR/pytorch-CycleGAN-and-pix2pix/checkpoints_scripted/production_G_A.pt" # replace with your own G_A model path
-        self.model_path_G_B = r"/home/david/workingDIR/pytorch-CycleGAN-and-pix2pix/checkpoints_scripted/production_G_B.pt" # replace with your own G_B model path
-        self.model_name = "cGAN_v1"
-        self.inference_type = "fake_B"  # Options: 'fake_B', 'rec_A', 'fake_A', 'rec_B'
+        self.destination_folder = r"/home/ychen/Documents/project/Data-Project/results/DCIS_IDC_cyclegan/DCIS_IDC20240826CycleGAN_DCIS+splitedData"  # replace with your own destination path
+        self.root_test_folder_A = r"/home/ychen/Documents/project/Data-Project/datasets/object_dection_data/DCIS+_images_split_feature_wise"  # replace with your own domain A path
+        self.root_test_folder_B = ""  # replace with your own domain B path
+        self.model_path_G_A = r"/home/ychen/Documents/project/Data-Project/checkpoints_scripted/cycleGAN/DCIS_IDC_cyclegan_G_A_checkpoints_scriptedlatest.pt"  # replace with your own G_A model path
+        self.model_path_G_B = r"/home/ychen/Documents/project/Data-Project/checkpoints_scripted/cycleGAN/DCIS_IDC_cyclegan_G_B_checkpoints_scriptedlatest.pt"  # replace with your own G_B model path
+        self.model_name = "cycleGAN_DCIS_IDC"
+        self.inference_types = ["fake_B", "rec_A", "fake_A", "rec_B"]  # Options: 'fake_B', 'rec_A', 'fake_A', 'rec_B'
         self.cycle_gan = True  # Option to decide if CycleGAN model is used
+        self.preserve_structure = True  # Option to preserve original folder structure
+        self.compress_results = True  # Option to compress the destination folder after processing
+
 
 def main():
     """Main function to run the processing"""
     cfg = Constants()
     processing(cfg)
 
+
 def load_model(cfg, device):
-    """Load appropriate models based on the inference type."""
+    """Load appropriate models based on the inference types."""
     netG_A, netG_B = None, None
     if cfg.cycle_gan:
-        if cfg.inference_type in ['fake_B', 'rec_A']:
+        if any(inf_type in ['fake_B', 'rec_A'] for inf_type in cfg.inference_types):
             netG_A = torch.jit.load(cfg.model_path_G_A)
             netG_A.to(device)
             netG_A.eval()
-        if cfg.inference_type in ['fake_A', 'rec_B']:
+        if any(inf_type in ['fake_A', 'rec_B'] for inf_type in cfg.inference_types):
             netG_B = torch.jit.load(cfg.model_path_G_B)
             netG_B.to(device)
             netG_B.eval()
     return netG_A, netG_B
+
 
 def process_image(image_path, device):
     """Load and transform an image for model inference."""
@@ -76,43 +79,69 @@ def process_image(image_path, device):
     tensor_images = torch.unsqueeze(tensor_images, dim=0)  # add a dimension for the batch
     return tensor_images.to(device)
 
-def run_inference(cfg, netG_A, netG_B, input_dir, device, destination_test_folder):
-    """Run inference based on the specified inference type."""
+
+def run_inference(cfg, netG_A, netG_B, input_dir, device, destination_test_folder, inference_types):
+    """Run inference based on the specified inference types."""
     with torch.no_grad():
-        for file in os.listdir(input_dir):
-            if file.endswith('.jpeg') or file.endswith('.png'):
-                image_path = os.path.join(input_dir, file)
-                tensor_images = process_image(image_path, device)
+        for root, _, files in os.walk(input_dir):
+            for file in files:
+                if file.endswith('.jpeg') or file.endswith('.png'):
+                    image_path = os.path.join(root, file)
+                    tensor_images = process_image(image_path, device)
 
-                with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA] if torch.cuda.is_available() else [ProfilerActivity.CPU], record_shapes=True) as prof:
-                    with record_function("model_inference"):
-                        if cfg.cycle_gan:
-                            if cfg.inference_type == 'fake_B':
-                                output = netG_A(tensor_images)  # G_A(A)
-                                suffix = '_fakeB'
-                            elif cfg.inference_type == 'rec_A':
-                                fake_B = netG_A(tensor_images)  # G_A(A)
-                                output = netG_B(fake_B)           # G_B(G_A(A))
-                                suffix = '_recA'
-                            elif cfg.inference_type == 'fake_A':
-                                output = netG_B(tensor_images)  # G_B(B)
-                                suffix = '_fakeA'
-                            elif cfg.inference_type == 'rec_B':
-                                fake_A = netG_B(tensor_images)  # G_B(B)
-                                output = netG_A(fake_A)           # G_A(G_B(B))
-                                suffix = '_recB'
-                            else:
-                                raise ValueError("Invalid inference type. Options are: 'fake_B', 'rec_A', 'fake_A', 'rec_B'")
+                    # Determine the relative path for preserving folder structure
+                    relative_path = os.path.relpath(root, input_dir)
+                    dest_folder = os.path.join(destination_test_folder, relative_path) if cfg.preserve_structure else destination_test_folder
+                    path_creator(dest_folder)
 
-                        # Convert tensor to numpy and save the output image
-                        output = np.squeeze(output.cpu().data.numpy())
-                        output_file = os.path.splitext(file)[0] + suffix + os.path.splitext(file)[1]
-                
-                plt.imsave(os.path.join(destination_test_folder, output_file), output, cmap='gray')
+                    # Copy the original image to the destination folder
+                    shutil.copy(image_path, os.path.join(dest_folder, file))
+
+                    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA] if torch.cuda.is_available() else [ProfilerActivity.CPU], record_shapes=True) as prof:
+                        with record_function("model_inference"):
+                            output_dict = {}
+                            for inference_type in inference_types:
+                                if cfg.cycle_gan:
+                                    if inference_type == 'fake_B':
+                                        output = netG_A(tensor_images)  # G_A(A)
+                                        suffix = '_fakeB'
+                                    elif inference_type == 'rec_A':
+                                        fake_B = netG_A(tensor_images)  # G_A(A)
+                                        output = netG_B(fake_B)           # G_B(G_A(A))
+                                        suffix = '_recA'
+                                    elif inference_type == 'fake_A':
+                                        output = netG_B(tensor_images)  # G_B(B)
+                                        suffix = '_fakeA'
+                                    elif inference_type == 'rec_B':
+                                        fake_A = netG_B(tensor_images)  # G_B(B)
+                                        output = netG_A(fake_A)           # G_A(G_B(B))
+                                        suffix = '_recB'
+                                    else:
+                                        raise ValueError("Invalid inference type. Options are: 'fake_B', 'rec_A', 'fake_A', 'rec_B'")
+
+                                # Convert tensor to numpy and save the output image
+                                output = np.squeeze(output.cpu().data.numpy())
+                                output_file = os.path.splitext(file)[0] + suffix + os.path.splitext(file)[1]
+                                plt.imsave(os.path.join(dest_folder, output_file), output, cmap='gray')
+                                output_dict[inference_type] = output_file
 
     # Print profiling information
     if 'prof' in locals():
         print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+
+
+def compress_results(cfg, destination_test_folder):
+    """Compress the destination folder if the flag is set."""
+    if cfg.compress_results:
+        zip_path = f"{destination_test_folder}.zip"
+        with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, _, files in os.walk(destination_test_folder):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    arcname = os.path.relpath(file_path, start=destination_test_folder)
+                    zipf.write(file_path, arcname)
+        print(f"Results compressed to: {zip_path}")
+
 
 def processing(cfg):
     input_dir_A = cfg.root_test_folder_A
@@ -124,26 +153,32 @@ def processing(cfg):
     if torch.cuda.is_available():
         print('GPU being used: ', torch.cuda.get_device_name(0))
 
-    # Load appropriate model based on inference type
+    # Load appropriate model based on inference types
     netG_A, netG_B = load_model(cfg, device)
 
     # Check if input directories exist
     try:
-        if cfg.inference_type in ['fake_B', 'rec_A'] and not os.path.exists(input_dir_A):
+        if not os.path.exists(input_dir_A) and any(inf_type in ['fake_B', 'rec_A'] for inf_type in cfg.inference_types):
             raise FileNotFoundError(f"Input directory for domain A not found: {input_dir_A}")
-        if cfg.inference_type in ['fake_A', 'rec_B'] and not os.path.exists(input_dir_B):
+        if not os.path.exists(input_dir_B) and any(inf_type in ['fake_A', 'rec_B'] for inf_type in cfg.inference_types):
             raise FileNotFoundError(f"Input directory for domain B not found: {input_dir_B}")
     except FileNotFoundError as e:
         print(e)
         sys.exit(1)
 
     # Run inference for domain A
-    if cfg.inference_type in ['fake_B', 'rec_A']:
-        run_inference(cfg, netG_A, netG_B, input_dir_A, device, destination_test_folder)
+    domain_A_inference_types = [inf for inf in cfg.inference_types if inf in ['fake_B', 'rec_A']]
+    if domain_A_inference_types:
+        run_inference(cfg, netG_A, netG_B, input_dir_A, device, destination_test_folder, domain_A_inference_types)
 
     # Run inference for domain B
-    if cfg.inference_type in ['fake_A', 'rec_B']:
-        run_inference(cfg, netG_A, netG_B, input_dir_B, device, destination_test_folder)
+    domain_B_inference_types = [inf for inf in cfg.inference_types if inf in ['fake_A', 'rec_B']]
+    if domain_B_inference_types:
+        run_inference(cfg, netG_A, netG_B, input_dir_B, device, destination_test_folder, domain_B_inference_types)
+
+    # Compress the results if the option is enabled
+    compress_results(cfg, destination_test_folder)
+
 
 if __name__ == "__main__":
     main()
